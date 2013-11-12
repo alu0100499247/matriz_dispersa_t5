2bc927b38d4f62e8ea156e2c504e9e407dfbf4fd
Francisco Palacios <alu0100503623@ull.edu.es>
2013-11-12 23:39:50 +0000

    Requires y ficheros agregados

--- lib/matriz_dispersa_t5.rb
+++ lib/matriz_dispersa_t5.rb
@@ -1,4 +1,7 @@
 require "matriz_dispersa_t5/version"
+require "matriz_dispersa_t5/prac6.rb"
+require "matriz_dispersa_t5/datos.dat"
+require "matriz_dispersa_t5/prac8.rb"
 
 module MatrizDispersaT5
   # Your code goes here...
